fdb2527ff7f7ad195652aca8e8e0107fd80acfa0
fuchst1 <55794984+fuchst1@users.noreply.github.com>
2026-02-04 16:14:04 +0100

    Add property management models and admin

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Owner, Ownership, Property, Unit
+
+
+class OwnershipInline(admin.TabularInline):
+    model = Ownership
+    extra = 0
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "zip_code", "street_address", "heating_share_percent")
+    search_fields = ("name", "city", "zip_code", "street_address")
+    inlines = (OwnershipInline,)
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "property",
+        "unit_type",
+        "door_number",
+        "usable_area",
+        "operating_cost_share",
+    )
+    list_filter = ("unit_type", "property")
+    search_fields = ("name", "door_number", "property__name")
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone")
+    search_fields = ("name", "email", "phone")
+    inlines = (OwnershipInline,)
+
+
+@admin.register(Ownership)
+class OwnershipAdmin(admin.ModelAdmin):
+    list_display = ("property", "owner", "share_percent")
+    search_fields = ("property__name", "owner__name")
+    list_filter = ("property", "owner")
